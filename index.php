6eaa9347ffa074d0940690f540fef83fbf5e6c93
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-27 14:03:16 +0200

    exo5p5 php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  $departements = array(
+    'Aisne' => 02,
+    'Nord' => 59,
+    'Oise' => 60,
+    'Pas-de-Calais' => 62,
+    'Somme' => 80);
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 5</title>
+  </head>
+  <body>
+
+  </body>
+</html>
